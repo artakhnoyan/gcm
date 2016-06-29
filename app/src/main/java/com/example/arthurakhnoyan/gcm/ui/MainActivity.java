d13f7111027aacb4ab12fa5ef9ad0710b5bad660
arthurakhnoyan <arthur.akhnoyan@picsar.com>
2016-06-29 11:30:44 +0400

    little design and editText closing with enter

--- app/src/main/java/com/example/arthurakhnoyan/gcm/ui/MainActivity.java
+++ app/src/main/java/com/example/arthurakhnoyan/gcm/ui/MainActivity.java
@@ -4,14 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.inputmethodservice.KeyboardView;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.arthurakhnoyan.gcm.R;
@@ -62,19 +66,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         Toast.makeText(getApplicationContext(), "This device does not support for Google Play Service!", Toast.LENGTH_LONG).show();
                     }
                 } else {
-                    String username = ((EditText) findViewById(R.id.username)).getText().toString();
-                    String phone = ((EditText) findViewById(R.id.phone)).getText().toString();
-                    Intent intent = new Intent(this, GCMRegistrationService.class);
-                    intent.putExtra("username", username);
-                    intent.putExtra("phone", phone);
-                    startService(intent);
+                    if(isNetworkAvailable()) {
+                        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+                        String phone = ((EditText) findViewById(R.id.phone)).getText().toString();
+                        Intent intent = new Intent(this, GCMRegistrationService.class);
+                        intent.putExtra("username", username);
+                        intent.putExtra("phone", phone);
+                        startService(intent);
+
+                        Intent sendingIntent = new Intent(this, UsersActivity.class);
+                        startActivity(sendingIntent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "You are not connected to internet", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
-                if(isNetworkAvailable()) {
-                    Intent intent = new Intent(this, UsersActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(MainActivity.this, "You are not connected to internet", Toast.LENGTH_SHORT).show();
-                }
+
                 break;
         }
     }
